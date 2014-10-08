3e3986d2bca3a81c933a69464c2a9158986cf6ba
danny <dw_@outlook.com>
2014-10-07 20:32:09 -0500

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,10 +1,14 @@
-﻿namespace app
+﻿using System;
+using Rhino.Mocks.Constraints;
+
+namespace app
 {
-  public class Calculator
-  {
-    public int add(int first, int second)
+    public class Calculator
     {
-      return first + second;
+        public int add(int first, int second)
+        {
+            if (first < 0 || second < 0) throw new ArgumentException();
+            return first + second;
+        }
     }
-  }
 }
